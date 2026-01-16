9cf3237fb96500c4c70c9a0471e4c9b203ad3091
rajramk12 <raj.ramk12@outlook.com>
2026-01-17 00:12:12 +0530

    err-model

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,10 +2,13 @@ class Stock < ApplicationRecord
   belongs_to :exchange
   has_many :daily_prices, -> { order(date: :desc) }, dependent: :destroy
 
-  scope :latest_price
+  validates :symbol, presence: true, uniqueness: { case_sensitive: false }
+  validates :name, presence: true
+  validates :isin, uniqueness: { allow_blank: true, case_sensitive: false }
+
   scope :by_symbol, ->(symbol) { where(symbol: symbol) }
 
-  def latcest_price
+  def latest_price
     self.daily_prices.order(date: :desc).first
   end
 
