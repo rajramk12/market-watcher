eeaecc7381b0f669fb2ead7d0d87d2b88f3c91a8
rajramk12 <raj.ramk12@outlook.com>
2026-01-20 00:43:21 +0530

    v2

--- db/migrate/20251220101855_create_stocks.rb
+++ db/migrate/20251220101855_create_stocks.rb
@@ -5,9 +5,9 @@ class CreateStocks < ActiveRecord::Migration[8.1]
       t.string :name
       t.string :isin
       t.boolean :active
-      t.precision :15, scale: 4 :price
-      t.precision :15, scale: 4 :d1_change
-      t.percentage :7, scale: 2 :d1_change_percent
+      t.decimal :price, precision: 20, scale: 4
+      t.decimal :d1_change, precision: 20, scale: 4
+      t.decimal :d1_change_percent, precision: 7, scale: 4
       t.json :mappings
       t.references :exchange, null: false, foreign_key: true
       t.timestamps
